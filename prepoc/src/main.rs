b3f3ac2a7e317dfb747f10644c0df900f043288b
Félix Raimundo <gamaz3ps@gmail.com>
2018-01-12 16:36:58 +0100

    WIP

--- prepoc/src/main.rs
+++ prepoc/src/main.rs
@@ -1,3 +1,38 @@
+use std::io;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::BufRead;
+
+struct VCFRecord {
+    chromosome: String,
+    pos: u64,
+    id: String,
+    reference: String,
+    alt: String,
+    quality: u32,
+    filter: String,
+    info: String
+}
+
+
+fn read_file() -> Result<(), io::Error> {
+    let f = try!(File::open("../data/PHASE3_SV_NA12878.vcf"));
+    let file = BufReader::new(&f);
+
+    let mut i : u32 = 0;
+
+    for line in file.lines() {
+        i += 1;
+        let l = line.unwrap();
+        let words : Vec<&str> = l.split('\t').collect();
+        println!("{:?}", words); 
+        if i > 10 {
+            break;
+        }
+    }
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _ = read_file();
 }
