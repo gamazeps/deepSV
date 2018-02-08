a56fb69a4da7ad6e7eec2f930f15c748ae2e4b04
Felix Raimundo <gamaz3ps@gmail.com>
2018-02-08 16:28:08 +0100

    add command for fetching the data

--- data/fetch_data.sh
+++ data/fetch_data.sh
@@ -5,3 +5,15 @@ estd219_1000_Genomes_Consortium_Phase_3_Integrated_SV.GRCh37.submitted.variant_c
 
 echo "Uncompress 1000GP vcf (GRCh37) file"
 gunzip estd219_1000_Genomes_Consortium_Phase_3_Integrated_SV.GRCh37.submitted.variant_call.germline.vcf.gz
+
+
+mkdir -p 'reads'
+
+#TODO(gamazeps): make sure to avoid the cram files as well as the exome alignments data.
+echo "Fetching the reads (bam) from 1000GP ftp"
+set e
+wget --recursive --continue \
+    --accept='*.mapped.*.bam*' \
+    --accept-regex='.*/alignment/' \
+    'ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/data/' \
+    --directory-prefix='reads
