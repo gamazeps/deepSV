31b3e84616f23b888f17838a62be352fa6ded95f
Félix Raimundo <gamaz3ps@gmail.com>
2018-02-10 00:53:48 +0100

    remove useless warning

--- preproc/src/consts.rs
+++ preproc/src/consts.rs
@@ -1,10 +1,13 @@
+#[allow(unused)]
 pub static FULL_1000GP_VCF_PATH: &'static str = "/data/fraimund/ftp.ncbi.nlm.nih.gov/pub/dbVar
     /data/Homo_sapiens/by_study/estd219_1000_Genomes_Consortium_Phase_3_Integrated_SV/vcf\
     /estd219_1000_Genomes_Consortium_Phase_3_Integrated_SV.GRCh37\
     .submitted.variant_call.germline.vcf";
 
+#[allow(unused)]
 pub static NA12878_VCF_PATH: &'static str = "../data/PHASE3_SV_NA12878.vcf";
 
+#[allow(unused)]
 pub static NA12878_BAM_PATH: &'static str = "/data/fraimund/ftp.1000genomes.ebi.ac.uk/vol1\
     /ftp/phase3/data/NA12878/alignment\
     /NA12878.mapped.ILLUMINA.bwa.CEU.low_coverage.20121211.bam";
